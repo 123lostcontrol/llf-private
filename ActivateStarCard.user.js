facd5b91d3ae7da24aaf9d270c29b5e6a15aaef5
123lostcontrol <113809426+123lostcontrol@users.noreply.github.com>
2022-09-25 05:07:49 -0700

    Optimize ActivateStarCard

--- ActivateStarCard.user.js
+++ ActivateStarCard.user.js
@@ -19,10 +19,27 @@
         return;
     }
     $('#activeBtn').before("<button class='btn btn-h45' id='batch-activate-btn'>批量激活</button>");
-    $('#batch-activate-btn').before("<button class='btn btn-h45' id='one-click-activate-btn'>一键激活</button>");
+    $('#batch-activate-btn').before("<button class='btn btn-h45' id='read-card-btn'>读取卡号</button>");
+    $('#read-card-btn').before('<textarea id="card-numbers" rows="5" cols="15" style="background: white" placeholder="粘贴需激活星梦卡号 每行一个"></textarea>');
+    $('#card-numbers').after("<div id='confirm-card-count'>已读取星梦卡 <span id='card-count' style='color: blue; font-weight:bold'>0</span> 张 | 已提交 <span id='completed-card-count' style='color: green; font-weight:bold'>0</span> 张</div>");
 
-    const cardNumbers = [];
+    var cardNumbers = [];
     var i = 0;
+
+    $('#read-card-btn').click(function(){
+        cardNumbers = [];
+        i = 0;
+        $.each($('#card-numbers').val().trim().split(/\n/), function(i, line){
+            if(line.trim()){
+                cardNumbers.push(line.trim());
+            }
+        });
+        $('#card-count').text(cardNumbers.length);
+        $('#completed-card-count').text(i);
+        console.log("Card numbers length: " + cardNumbers.length);
+        console.log("Card numbers saved: ");
+        console.log(cardNumbers);
+    });
 
     $('#batch-activate-btn').click(function(){
         if (i < cardNumbers.length) {
@@ -30,13 +47,11 @@
             $('#code').val(cardNumbers[i]);
             $('#activeBtn').click();
             window.setTimeout(function() {$('.layui-m-layercont .btn-close').click();}, 1000);
+            window.setTimeout(function() {$('#batch-activate-btn').click();}, 2000);
             i++;
+            $('#completed-card-count').text(i);
         } else {
             window.alert("当前卡组已全部激活完成");
         }
-    });
-
-    $('#one-click-activate-btn').click(function(){
-        window.setInterval(function () {$('#batch-activate-btn').click();}, 2000);
     });
 })();
